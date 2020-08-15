83318860422e82f8d90fe396a2c4fea690943942
Mugilan-Codes <mugilancodes@gmail.com>
2020-08-15 11:25:22 +0530

    Remove OMZ install script

--- fresh.sh
+++ fresh.sh
@@ -12,7 +12,7 @@ fi
 brew update
 
 # Install Oh-My-Zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install all our dependencies with bundle (See Brewfile)
 brew tap homebrew/bundle
