46f06ba53616b6aff38db859402378938eda9e9d
Mugilan-Codes <mugilancodes@gmail.com>
2020-08-18 19:57:09 +0530

    removed omz from dotfiles

--- fresh.sh
+++ fresh.sh
@@ -11,12 +11,6 @@ fi
 # Update Homebrew recipes
 brew update
 
-echo "Installing Oh-My-Zsh..."
-
-# # Install Oh-My-Zsh
-# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-
 echo "Installing Brew Files..."
 # Install all our dependencies with bundle (See Brewfile)
 brew tap homebrew/bundle
