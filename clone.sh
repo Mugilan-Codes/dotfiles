cc79040bc7b7fbe3cc4a1564e5fd8d283adad959
Mugilan <mugilancodes@gmail.com>
2020-10-25 18:41:43 +0530

    More Work

--- clone.sh
+++ clone.sh
@@ -6,14 +6,17 @@ mkdir -p $HOME/Projects/{Forks,College,Playground,Repos,Personal,Tutorials}
 
 echo "Cloning repositories..."
 
+
 PROJECTS=$HOME/Projects
 
-PERSONAL=$PROJECTS/Personal
+FORKS=$PROJECTS/Forks
+COLLEGE=$PROJECTS/College
+PLAYGROUND=$PROJECTS/Playground
 REPOS=$PROJECTS/Repos
+PERSONAL=$PROJECTS/Personal
+TUTORIALS=$PROJECTS/Tutorials
 
 # Personal
-git clone git@github.com:Mugilan-Codes/100-days-of-code.git $PERSONAL/100-days-of-code
-git clone git@github.com:Mugilan-Codes/workout-tracker.git $PERSONAL/workout-tracker
 
 # Repos
 git clone git@github.com:Mugilan-Codes/my-family-budget.git $REPOS/my-family-budget
