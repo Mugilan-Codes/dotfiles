68433f8e6484b64a27336a868c630dd06251417d
Mugilan <mugilancodes@gmail.com>
2020-10-25 18:09:20 +0530

    Remove Duplicates in PATH

--- zsh/custom/path.zsh
+++ zsh/custom/path.zsh
@@ -10,3 +10,6 @@ export PATH="$HOME/.node/bin:$PATH"
 
 # Use project specific binaries before global ones
 export PATH="node_modules/.bin:vendor/bin:$PATH"
+
+# Remove Duplicates in PATH - (https://superuser.com/a/1321712)
+export PATH="$(perl -e 'print join(":", grep { not $seen{$_}++ } split(/:/, $ENV{PATH}))')"
