c15b76e1462eb0c68bfbb70f558d93ae169e76f4
Mugilan <mugilancodes@gmail.com>
2020-10-29 14:17:54 +0530

    Add Git Functions

--- zsh/custom/functions.zsh
+++ zsh/custom/functions.zsh
@@ -41,3 +41,17 @@ update_all() {
   
   npm_update
 }
+
+# Print short status and log of latest commits:
+git_status_short() {
+  if [[ -z $(git status -s) ]]; then
+    echo 'Nothing to commit, working tree clean\n'
+  else
+    git status -s && echo ''
+  fi
+  git log -${1:-3} --oneline | cat
+}
+
+git_count() {
+  echo "$(git rev-list --count HEAD) commits total up to current HEAD"
+}
