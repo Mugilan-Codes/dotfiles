8f067c4a066d399f84330fe558d0ce30d494cef6
Mugilan <mugilancodes@gmail.com>
2020-12-16 13:36:24 +0530

    Android Added to PATH

--- zsh/custom/path.zsh
+++ zsh/custom/path.zsh
@@ -13,5 +13,12 @@ export PATH="$HOME/.node/bin:$PATH"
 # Use project specific binaries before global ones
 export PATH="node_modules/.bin:vendor/bin:$PATH"
 
+# React Native Android
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/tools/bin
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+
 # Remove Duplicates in PATH - (https://superuser.com/a/1321712)
 export PATH="$(perl -e 'print join(":", grep { not $seen{$_}++ } split(/:/, $ENV{PATH}))')"
